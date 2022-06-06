2d57c27e01880cce8990b7463ce7a32b1a6a39ad
freQniK <freQniK@protonmail.com>
2022-06-05 20:48:43 -0400

    da node upgrad0r

--- upgrade_dvpn_node.sh
+++ upgrade_dvpn_node.sh
@@ -9,23 +9,23 @@
 #
 # May need to be edited to suit others needs
 
-# EDIT THESE to all your subdomains (nodes) of your primary domain (url)
-
-nodes=('poincare' 'bernoulli')
+# EDIT THESE to all your subdomains (nodes) of your primary domain (url) and ssh port (port)
+nodes=('cantor' 'erdos' 'fibonacci' 'hecke' 'lobachevsky' 'molien' 'riemann' 'schrodinger' 'shafarevich' 'shimura' 'thabit' 'zwicky' 'bolzano' )
 url='mathnodes.com' 
+port='22000'
 
 for node in ${nodes[@]}; do
         url_node="$node.$url"
         echo "$url_node"
 
-        ssh -p 22000 sentinel@$url_node << EOF
+        ssh -p $port sentinel@$url_node << EOF
         rm -rf ~/dvpn-node
         git clone https://github.com/sentinel-official/dvpn-node 
 EOF
 
-        ssh -p 22000 sentinel@$url_node 'bash -s' < docker_ps.sh
+        ssh -p $port sentinel@$url_node 'bash -s' < docker_ps.sh
 
-        ssh -p 22000 sentinel@$url_node << EOF
+        ssh -p $port sentinel@$url_node << EOF
         cd dvpn-node
         git checkout v0.3.2 
         docker build --file Dockerfile --tag sentinel-dvpn-node  --force-rm  --no-cache  --compress .
