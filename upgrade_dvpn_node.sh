90208e05dec5523987e7f8c9c7e2e31ffb9d32af
freQniK <freQniK@protonmail.com>
2022-06-29 23:07:42 -0400

    Dynamic username

--- upgrade_dvpn_node.sh
+++ upgrade_dvpn_node.sh
@@ -13,19 +13,20 @@
 nodes=('cantor' 'erdos' 'fibonacci' 'hecke' 'lobachevsky' 'molien' 'riemann' 'schrodinger' 'shafarevich' 'shimura' 'thabit' 'zwicky' 'bolzano' )
 url='mathnodes.com' 
 port='22000'
+username='sentinel'
 
 for node in ${nodes[@]}; do
         url_node="$node.$url"
         echo "$url_node"
 
-        ssh -p $port sentinel@$url_node << EOF
+        ssh -p $port $username@$url_node << EOF
         rm -rf ~/dvpn-node
         git clone https://github.com/sentinel-official/dvpn-node 
 EOF
 
-        ssh -p $port sentinel@$url_node 'bash -s' < docker_ps.sh
+        ssh -p $port $username@$url_node 'bash -s' < docker_ps.sh
 
-        ssh -p $port sentinel@$url_node << EOF
+        ssh -p $port $username@$url_node << EOF
         cd dvpn-node
         git checkout v0.3.2 
         docker build --file Dockerfile --tag sentinel-dvpn-node  --force-rm  --no-cache  --compress .
