2228a16661baac2de5dbbb33a8a9d37a54e3206d
freqnik <freqnik@protonmail.com>
2023-01-29 20:08:36 -0500

    Updated to use IP addresses

--- upgrade_dvpn_node.sh
+++ upgrade_dvpn_node.sh
@@ -6,17 +6,24 @@
 # Also need 'sentinel ALL=(ALL:ALL) NOPASSWD: ALL' in /etc/sudoers
 # This can be edited afterwards to remove the NOPASSWD permissions
 #
+# ssh pubkey access needs to be added to your /etc/ssh/sshd_config file
+# for this to work
 #
 # May need to be edited to suit others needs
 
-# EDIT THESE to all your subdomains (nodes) of your primary domain (url) and ssh port (port)
-nodes=('cantor' 'erdos' 'fibonacci' 'hecke' 'lobachevsky' 'molien' 'riemann' 'schrodinger' 'shafarevich' 'shimura' 'thabit' 'zwicky' 'bolzano' )
-url='mathnodes.com' 
+# EDIT THESE to all your IP ADDRESSES of your nodes
+nodes=('x.x.x.x' 'y.y.y.y' 'z.z.z.z' 'w.w.w.w')
+#url='mathnodes.com' 
+# SSH PORT (EDIT)
 port='22000'
+# USERNAME OF DVPN NODE SOFTWARE
 username='sentinel'
+# DVPN NODE VERSION TO USE
+dvpn_version='v0.4.0'
 
 for node in ${nodes[@]}; do
-        url_node="$node.$url"
+        #url_node="$node.$url"
+        url_node="$node"
         echo "$url_node"
 
         ssh -p $port $username@$url_node << EOF
@@ -28,7 +35,7 @@ EOF
 
         ssh -p $port $username@$url_node << EOF
         cd dvpn-node
-        git checkout v0.3.2 
+        git checkout $dvpn_version
         docker build --file Dockerfile --tag sentinel-dvpn-node  --force-rm  --no-cache  --compress .
 EOF
         
